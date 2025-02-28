87dc0333fa83e56e1cb5218202b505ae1c11087e
noname9586 <gurovkirill29@gmail.com>
2025-02-28 09:55:38 +0300

    Add files via upload

---
+++ Угадай число.py
@@ -0,0 +1,51 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import random
+
+def generate_number(lower, upper):
+    return random.randint(lower, upper)
+
+def get_guess():
+    while True:
+        try:
+            guess = int(input("Введите ваше предположение: "))
+            return guess
+        except ValueError:
+            print("Пожалуйста, введите целое число.")
+... 
+... def play_game(lower, upper, max_attempts):
+...     number_to_guess = generate_number(lower, upper)
+...     attempts = 0
+... 
+...     print(f"Я загадал число от {lower} до {upper}. У вас есть {max_attempts} попыток.")
+... 
+...     while attempts < max_attempts:
+...         guess = get_guess()
+...         attempts += 1
+...         
+...         if guess < number_to_guess:
+...             print("Больше!")
+...         elif guess > number_to_guess:
+...             print("Меньше!")
+...         else:
+...             print(f"Поздравляю! Вы угадали число {number_to_guess} за {attempts} попыток.")
+...             return
+... 
+...     print(f"Вы исчерпали все попытки! Загаданное число было {number_to_guess}.")
+... 
+... def main():
+...     print("Добро пожаловать в игру 'Угадай число'!")
+... 
+...     while True:
+...         lower = int(input("Введите нижнюю границу диапазона: "))
+...         upper = int(input("Введите верхнюю границу диапазона: "))
+...         max_attempts = int(input("Введите максимальное количество попыток: "))
+...         
+...         play_game(lower, upper, max_attempts)
+... 
+...         play_again = input("Хотите сыграть еще раз? (да/нет): ").strip().lower()
+...         if play_again != 'да':
+...             break
+... 
+... if __name__ == "__main__":
+...     main()
